4c1fe9f6be9140b15cfc7862e2bc7b4f085039a7
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-22 09:37:54 -0700

    change 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 const list = [
   {
@@ -16,14 +16,19 @@ const list = [
     fighter_status: "Active",
     rank: "C",
     pound_for_pound_rank: null,
-    thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252FKhabib-Nurmagomedov%252FKhabib-Nurmagomedov_232925_medium_thumbnail.jpg?mw300-mh300-tc1",
-    belt_thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FKhabib_Nurmagomedov%252FNURMAGOMEDOV_KHABIB_BELT2.png?mw300-mh300-tc1",
-    left_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252F1%252F229%252FDoubleheadsPNGs%252FNURMAGOMEDOV_KHABIB_L.png?mh530",
-    right_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FKhabib_Nurmagomedov%252FNURMAGOMEDOV_KHABIB_R223.png?mh530",
-    profile_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252F1%252F229%252FHeadshotPNGs%252FNURMAGOMEDOV_KHABIB.png?mw300-mh300-tc1",
+    thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252FKhabib-Nurmagomedov%252FKhabib-Nurmagomedov_232925_medium_thumbnail.jpg?mw300-mh300-tc1",
+    belt_thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FKhabib_Nurmagomedov%252FNURMAGOMEDOV_KHABIB_BELT2.png?mw300-mh300-tc1",
+    left_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252F1%252F229%252FDoubleheadsPNGs%252FNURMAGOMEDOV_KHABIB_L.png?mh530",
+    right_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FKhabib_Nurmagomedov%252FNURMAGOMEDOV_KHABIB_R223.png?mh530",
+    profile_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252F1%252F229%252FHeadshotPNGs%252FNURMAGOMEDOV_KHABIB.png?mw300-mh300-tc1",
     link: "http://www.ufc.com/fighter/Khabib-Nurmagomedov"
-    },
-    {
+  },
+  {
     id: 241944,
     nickname: "The Chosen One",
     wins: 19,
@@ -37,14 +42,19 @@ const list = [
     fighter_status: "Active",
     rank: "C",
     pound_for_pound_rank: null,
-    thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252FTyron-Woodley%252FTyron-Woodley_241944_medium_thumbnail.jpg?mw300-mh300-tc1",
-    belt_thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L-PRINT.png?mw300-mh300-tc1",
-    left_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L_BELT_S.png?mh530",
-    right_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L_BELT_S.png?mh530",
-    profile_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F1%252F228%252FHeadshotPNGs%252FWOODLEY_TYRON_BELT.png?mw300-mh300-tc1",
+    thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252FTyron-Woodley%252FTyron-Woodley_241944_medium_thumbnail.jpg?mw300-mh300-tc1",
+    belt_thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L-PRINT.png?mw300-mh300-tc1",
+    left_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L_BELT_S.png?mh530",
+    right_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FTyrone_Woodley%252FWOODLEY_TYRON_L_BELT_S.png?mh530",
+    profile_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252F1%252F228%252FHeadshotPNGs%252FWOODLEY_TYRON_BELT.png?mw300-mh300-tc1",
     link: "http://www.ufc.com/fighter/Tyron-Woodley"
-    },
-    {
+  },
+  {
     id: 290162,
     nickname: "The Reaper",
     wins: 21,
@@ -58,30 +68,33 @@ const list = [
     fighter_status: "Active",
     rank: "C",
     pound_for_pound_rank: null,
-    thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252Frobert-whittaker%252Frobert-whittaker_290162_medium_thumbnail.jpg?mw300-mh300-tc1",
-    belt_thumbnail: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_BELT-MOCK2.png?mw300-mh300-tc1",
-    left_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_L.png?mh530",
-    right_full_body_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_R.png?mh530",
-    profile_image: "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT.png?mw300-mh300-tc1",
+    thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Fgenerated_images_sorted%252FFighter%252Frobert-whittaker%252Frobert-whittaker_290162_medium_thumbnail.jpg?mw300-mh300-tc1",
+    belt_thumbnail:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_BELT-MOCK2.png?mw300-mh300-tc1",
+    left_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_L.png?mh530",
+    right_full_body_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT_R.png?mh530",
+    profile_image:
+      "http://imagec.ufc.com/http%253A%252F%252Fmedia.ufc.tv%252Ffighter_images%252FRobert_Whittaker%252FWHITTAKER_ROBERT.png?mw300-mh300-tc1",
     link: "http://www.ufc.com/fighter/robert-whittaker"
-    },
-
-]
+  }
+];
 
 function isSearched(searchTerm) {
-  return function (item) {
+  return function(item) {
     return item.last_name.toLowerCase().includes(searchTerm.toLowerCase());
-  }
+  };
 }
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       list,
-      searchTerm: ''
+      searchTerm: ""
     };
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
@@ -89,46 +102,43 @@ class App extends Component {
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
-
   }
 
   onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.id !== id);
-    this.setState({ list: updatedList })
+    this.setState({ list: updatedList });
   }
 
-  
-
   render() {
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
-      <form>
-          <input type="text" onChange={this.onSearchChange}/>
-</form>
-      {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
-      <div key={item.id}>
-              <span>
-                <a href={item.thumbnail}>{item.last_name}</a>
-              </span>
-              <span>{item.first_name}</span>
-              <span>{item.weight_class}</span>
-              <span>{item.wins}</span><span>
-              <button
-                onClick={() => this.onDismiss(item.id)}
-                type="button"
-              >
-                
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item => (
+          <div key={item.id}>
+            <span>
+              <a href={item.thumbnail}>{item.last_name}</a>
+            </span>
+            <span>{item.first_name}</span>
+            <span>{item.weight_class}</span>
+            <span>{item.wins}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.id)} type="button">
                 Dismiss
               </button>
             </span>
-
-            </div>
-            )}
-    </div> )
-
+          </div>
+        ))}
+      </div>
+    );
   }
-
 }
-  
 
 export default App;
